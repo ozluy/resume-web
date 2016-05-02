a55998950b169ca67b921ad1c5019a2f2272857a
Yusuf Özlü <ozluyusuf@gmail.com>
2016-05-03 01:19:19 +0300

    aside
    aside animation change and blackout add

--- dev/assets/scripts/common.js
+++ dev/assets/scripts/common.js
@@ -3,6 +3,7 @@ console.log(ozluy);
 
 var hamburger=document.getElementById('hamburger_menu'),
 leftMenu=document.getElementById('left_menu'),
+header=document.getElementById('header'),
 footer=document.getElementById('footer'),
 main=document.getElementById('main');
 
@@ -12,11 +13,13 @@ var menuOpener = function(){
     leftMenu.className='';
     main.className='';
     footer.className='';
+    header.className='';
   }
   else{
     hamburger.className='hamburger clicked';
     leftMenu.className='opened';
     main.className='translated';
     footer.className='translated';
+    header.className='translated';
   }
 }
